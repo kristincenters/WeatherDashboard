9f80647020b4eda7371665efb58087698797c5a1
kcenters <kristincenters@gmail.com>
2020-03-16 15:18:57 -0500

    queryURL function

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,41 @@
-//capture user search text
-var locationInput = $('#search-term').val();
-//add api key
-var apiKEY = '36647473717c57282b7a80e5038cd6f3';
+//api key:&appid=36647473717c57282b7a80e5038cd6f3
 
-var queryURL =
-	'https://api.openweathermap.org/data/2.5/forecast?' + locationInput + apiKEY;
+var searchTerm = $('#findLocation');
+//var locationHistory = $('#priorLocation');
+var currentWeatherDay = $('#currentCity');
+var currentWeatherTemp = $('#currentTemp');
+var currentWeatherWind = $('#currentWind');
+var currentWeatherUV = $('#currentUV');
 
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-}).then(function(response) {
-	console.log(queryURL);
-	console.log(response);
-});
+var currentDate = moment().format('L');
+console.log(currentDate);
+
+function searchLocation(findLocation) {
+	//capture user search text
+	var locationInput = $('#searchTerm')
+		.val()
+		.trim();
+	//build query string with api key
+	var queryURL =
+		'https://api.openweathermap.org/data/2.5/weather?q=' +
+		locationInput +
+		'&appid=36647473717c57282b7a80e5038cd6f3';
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).then(function(response) {
+		console.log(queryURL);
+		console.log(response);
+	});
+	//searchLocation();
+
+	$('#findLocation').on('click'),
+		function(event) {
+			event.preventDefault();
+		};
+
+	// new div for city content
+	$('#currentCity').empty() + currentDate;
+}
+findLocation();
